530377a20213f4cf0526be3eeb011701d95ed87d
Robin Appelman <icewind@owncloud.com>
2016-04-09 16:08:03 +0200

    more agressive clean

--- clean.sh
+++ clean.sh
@@ -2,10 +2,14 @@
 
 cd $SERVER/tf2/tf/maps
 
-MAPS_KEEP=("cp_badlands.bsp" "cp_gravelpit.bsp" "cp_granary.bsp" "cp_gullywash_final1.bsp" "cp_process_final.bsp" "cp_snakewater_final1.bsp" "cp_steel.bsp" "koth_lakeside_final.bsp" "pl_badwater.bsp" "pl_borneo.bsp" "pl_upward.bsp")
+MAPS_KEEP=("cp_badlands.bsp")
 MAPS_ALL=( $(find -type f -name "*.bsp" -printf "%f\n") )
 
 # MAPS_DELETE=($(diff MAPS_ALL[@] MAPS_KEEP[@]))
 MAPS_DELETE=$(echo ${MAPS_ALL[@]} ${MAPS_KEEP[@]} | tr ' ' '\n' | sort | uniq -u)
 
 rm ${MAPS_DELETE[@]}
+
+cd $SERVER/tf2/tf
+
+rm resource/tf_*.txt
